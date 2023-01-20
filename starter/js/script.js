36a6a6a7dc5e529ad3e1aa46ae4a9c87c02acf2b
Ebele Okemba <okemba50@gmail.com>
2023-01-20 20:43:02 +0000

    create the dom element of the textarea and also, arrays of the dom element

--- starter/js/script.js
+++ starter/js/script.js
@@ -3,3 +3,26 @@ setInterval(
         $("#currentDay").text(moment().format("DDD MMM, YYYY hh:mm:ss"))
 
     }, 1000);
+
+    // Dom element of textarea
+let hourNine = $("09");
+let hourTen = $("10");
+let hourEleven = $("11");
+let hourTweleve = $("12");
+let hourOne = $("01");
+let hourTwo = $("02");
+let hourThree = $("03");
+let hourFour = $("04");
+let hourFive = $("05");
+
+// array element of the dom element of textarea
+let hourlyTime = [hourNine, hourTen, hourEleven, hourTweleve, hourOne, hourTwo, hourThree, hourFour, hourFive];
+let specificHour = parseInt(moment().format());
+
+
+
+
+    $("button").on("click", function (event) {
+        console.log($(event.target).siblings("textarea").val())
+        
+    })
