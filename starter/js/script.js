c9ccdc7e3b624432dff5b17b7a0574f305e6da69
Ebele Okemba <okemba50@gmail.com>
2023-01-21 21:55:18 +0000

    add a function that check the current time against the set time duration and assign colour based on the outcome

--- starter/js/script.js
+++ starter/js/script.js
@@ -19,7 +19,26 @@ let hourFive = $("#05");
 let hourlyTime = [hourNine, hourTen, hourEleven, hourTweleve, hourOne, hourTwo, hourThree, hourFour, hourFive];
 let specificHour = parseInt(moment().format("hh"));
 
+//function to assign the colour based on current, past and prsent
+function assignColour() {
+    for (let i = 0; i < hourlyTime.length; i++) {
+        const hourlyTimes = hourlyTime[i];
+        hourlyTimes.removeClass("past present current");
 
+        if (specificHour === hourlyTimes.data("hour")) {
+            hourlyTimes.addClass("present");
+        }
+        else if (specificHour > hourlyTimes.data("hour")){
+            hourlyTimes.addClass("past")
+        }
+        else{
+            hourlyTimes.addClass("future");
+        }
+        
+    }
+    
+}
+assignColour();
 
 
 
